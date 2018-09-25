9ec8e5185560a36ce28655a4a2ed7417d982ddfe
jezza23 <jezzairves@gmail.com>
2018-09-24 21:36:26 -0700

    Added Handling of Exiting TCP Connection & Fixed New Line issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path")
 const cmd = spawn(path.join(__dirname, "proc"), ["arg1", "arg2"]) // Change this...
 const net = require("net")
 
-const name = "Process Console"
+const name = "Modfinity Console"
 const ver = "1.0.0"
 
 let users = require("./users.json")
@@ -11,10 +11,12 @@ let users = require("./users.json")
 var clients = []
 
 cmd.stdout.on("data", e => {
-    process.stdout.write(e.toString() + "\n")
+    process.stdout.write(e.toString())
     if(clients.length >= 1) {
         clients.forEach(t => {
-            t.write(e.toString())
+                if(t.isLoggedin) {
+                        t.write(e.toString() + "\r")
+                }
         })
     }
 })
@@ -29,9 +31,9 @@ var server = net.createServer((socket) => {
     socket.isUsername = false
     clients.push(socket)
     console.log("[TCPStream] " + socket.localAddress + ":" + socket.localPort + " has connected")
-    socket.write("Connected to " + name + " v" + ver + "\nUsername: ")
+    socket.write("Connected to " + name + " v" + ver + "\r\nUsername: ")
     socket.on('end', () => {
-        clients.splice( clients.indexOf(socket) )
+        clients.splice(clients.indexOf(socket), 1)
     })
     socket.on("data", data => {
         if(socket.isLoggedin) {
@@ -48,21 +50,21 @@ var server = net.createServer((socket) => {
                 }
             })
             if(!user_contain) {
-                socket.write("User not found...\nUsername: ")
+                socket.write("User not found...\r\nUsername: ")
             } else {
                 socket.username = data.toString().replace("\r", "").replace("\n", "")
                 socket.isUsername = true
-                socket.write("Password: ") 
+                socket.write("Password: ")
             }
         } else if (socket.isUsername) {
             var auth = false
             users.forEach(user => {
-                if(user.user == socket.username && user.pass == data.toString().replace("\r", "").replace("\n", "")) { auth = true }  
+                if(user.user == socket.username && user.pass == data.toString().replace("\r", "").replace("\n", "")) { auth = true }
             })
             socket.isLoggedin = auth
             if(!auth) {
                 socket.isUsername = false
-                socket.write("Incorrect Password...\nUsername: ")
+                socket.write("Incorrect Password...\r\nUsername: ")
             }
         }
     })
